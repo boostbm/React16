6bd1aaee5f188212bd8f9fc3f7dc248ec2a9d411
Boost <boostbm@gmail.com>
2022-01-24 17:40:58 -0800

    Updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
+
+
+class App extends Component {
+
+  state = {
+    toggle: true
+  } 
+  toggle = () => {
+    this.setState({
+      toggle: !this.state.toggle
+    })
+    
+  }
+
+    render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Welcome text="Welcome to React JS"></Welcome>Edit <code>src/App.js</code> and save to reload.
         </p>
+        {this.state.toggle && 
+        <p>This should show and Hide</p>
+        } 
+        <button onClick={this.toggle}>Show / Hide</button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,5 +40,16 @@ function App() {
     </div>
   );
 }
+}
 
+
+class Welcome extends Component {
+
+  render() {
+    const { text } = this.props;
+    return (
+      <h1 className='App-title'>{text}</h1>
+    )
+  }
+}
 export default App;
